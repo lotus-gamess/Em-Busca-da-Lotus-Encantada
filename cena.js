e5893848ef1e619bf117272ca8e7414930af30ab
nicolysousaa <126250183+nicolysousaa@users.noreply.github.com>
2023-03-28 10:19:57 -0300

    edição

---
+++ cena.js
@@ -0,0 +1,20 @@
+export default class CenaDeAbertura extends Phaser.Scene {
+    constructor() {
+      super("abertura");
+    }
+  
+    preload() {
+        // Carregamento de arquivo/objetos em memória (desenhos aqui)
+    }
+
+  
+    create() {
+        // carregar em tela e/ou
+        // Registrar eventos 
+    }        
+
+  
+    update() {
+        // Código executado a cada frame
+    }
+  }
